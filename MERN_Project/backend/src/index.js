01d0b63f8b347f885f89d86a0dbd89762a0e1d2a
KellyYII <71577249+kellyyii@users.noreply.github.com>
2025-12-12 13:31:25 +0800

    Add files via upload

--- MERN_Project/backend/src/index.js
+++ MERN_Project/backend/src/index.js
@@ -1,26 +1,41 @@
-const express = require("express");
-const dotenv = require("dotenv").config();
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
-connectDB = require("./lib/db.js")
-const authRoutes = require("./routes/auth.route.js");
-const messageRoutes = require("./routes/message.route.js");
-const {app, server} = require("./lib/socket.js");
+import path from "path";
+
+import { connectDB } from "./lib/db.js";
+
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
+
+dotenv.config();
 
 const port = process.env.PORT || 5000;
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin: "http://localhost",
         credentials: true,
     })
 );
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
